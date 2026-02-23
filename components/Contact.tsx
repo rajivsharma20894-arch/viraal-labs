f05db812cb037f512762d069006a102eefb30f97
Rajiv Sharma <Rajiv.sharma20894@gmail.com>
2026-02-23 08:35:32 +0530

    Add Formspree form ID

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -23,7 +23,7 @@ export default function Contact() {
     setStatus("sending");
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/mykdbzdo", {
         method: "POST",
         headers: { "Content-Type": "application/json", Accept: "application/json" },
         body: JSON.stringify(formData),
--- components/ui/AuditModal.tsx
+++ components/ui/AuditModal.tsx
@@ -17,7 +17,7 @@ export default function AuditModal({ isOpen, onClose }: AuditModalProps) {
     setStatus("sending");
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/mykdbzdo", {
         method: "POST",
         headers: { "Content-Type": "application/json", Accept: "application/json" },
         body: JSON.stringify(formData),
